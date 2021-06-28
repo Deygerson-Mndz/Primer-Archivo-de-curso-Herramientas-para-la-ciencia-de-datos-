b0e90e95d5f424124485a0f1bb5b57e45fc75b7f
Deygerson-Mndz <74508196+Deygerson-Mndz@users.noreply.github.com>
2021-06-28 15:24:40 -0500

    Create Primero.py
    Creación del primer archivo.

---
+++ Primero.py
@@ -0,0 +1,3 @@
+#Primer archivo creado y subido a Github, para el curso "Herramientas para la ciencia de datos" de IBM.
+
+print ("Hola mundo, desde Github, rumbo a obtener la certificacion profesional de ciencia de datos de IBM")
